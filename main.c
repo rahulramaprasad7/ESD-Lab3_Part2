d44cd8172b482c2822a7fc72ba64f7054d739865
rahulramaprasad7 <54955817+rahulramaprasad7@users.noreply.github.com>
2019-10-24 00:07:02 -0600

    PWM Duty cycle working

--- main.c
+++ main.c
@@ -14,6 +14,20 @@ void main(void)
     P1->IFG = 0;                                    // Clear all P1 interrupt flags
     P1->IE |= BIT1 | BIT4;                          // Enable interrupt for P1.1
 
+    P1->DIR |= BIT0;
+    P1->OUT |= BIT0;
+
+    TIMER_A0->CCTL[0] |= TIMER_A_CCTLN_OUTMOD_3 | TIMER_A_CCTLN_CCIE; // TACCR0 interrupt enabled
+    TIMER_A0->CCTL[1] |= TIMER_A_CCTLN_OUTMOD_3 | TIMER_A_CCTLN_CCIE; // TACCR1 interrupt enabled
+
+    TIMER_A0->CCR[0] = 18750;
+    TIMER_A0->CCR[1] = 18750;
+
+    TIMER_A0->CTL = TIMER_A_CTL_SSEL__SMCLK | TIMER_A_CTL_MC__UP; // SMCLK, up mode
+
+    TIMER_A0->CTL |= TIMER_A_CTL_ID_2;       //Pre-scaler divide by 4
+    TIMER_A0->EX0 |= TIMER_A_EX0_TAIDEX_7;   //Prescaler divide by 8
+
     P2->DIR |= 0xFF; P2->OUT = 0;
     P3->DIR |= 0xFF; P3->OUT = 0;
     P4->DIR |= 0xFF; P4->OUT = 0;
@@ -24,7 +38,9 @@ void main(void)
     P9->DIR |= 0xFF; P9->OUT = 0;
     P10->DIR |= 0xFF; P10->OUT = 0;
 
-    NVIC->ISER[1] = 1 << ((PORT1_IRQn) & 31);
+    NVIC->ISER[1] |= 1 << ((PORT1_IRQn) & 31);
+    NVIC->ISER[0] |= (1 << (TA0_0_IRQn & 31)) | (1 << (TA0_N_IRQn & 31));
+
     __enable_irq();
 
     while(1)
@@ -37,14 +53,15 @@ void main(void)
 void PORT1_IRQHandler(void)
 {
     volatile uint32_t i;
-    printf("Entered interrupt\n");
 
     if(P1->IFG & BIT1)
     {
-        printf("PWM increase by 10%");
-        printf("\n");
+        //printf("PWM increase by 10% ");
+        TIMER_A0->CCR[0] += (18750 + 1875);
+        //printf("%d",TIMER_A0->CCR[0]);
+        //printf("\n");
         // Delay for P1.1 switch debounce
-        for(i = 0; i < 1000; i++)
+        for(i = 0; i < 10000; i++)
             P1->IFG &= ~BIT1;
     }
 
@@ -52,12 +69,25 @@ void PORT1_IRQHandler(void)
 
     else if(P1->IFG & BIT4)
     {
-        printf("PWM decrease by 10%\n");
-        printf("\n");
+        //printf("PWM decrease by 10% ");
+        TIMER_A0->CCR[1] += (18750);
+        //printf("%d",TIMER_A0->CCR[1]);
+        //printf("\n");
         // Delay for P1.4 switch debounce
-        for(i = 0; i < 1000; i++)
+        for(i = 0; i < 10000; i++)
             P1->IFG &= ~BIT4;
     }
 
 }
 
+void TA0_0_IRQHandler(void)
+{
+    TIMER_A0->CCTL[0] &= ~TIMER_A_CCTLN_CCIFG;
+    P1->OUT ^= BIT0;
+}
+
+void TA0_N_IRQHandler(void)
+{
+    TIMER_A0->CCTL[1] &= ~TIMER_A_CCTLN_CCIFG;
+    P1->OUT ^= BIT0;
+}
